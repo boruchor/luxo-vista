35c2d243c672011c24a6698ad609178409bdbbb4
boruchor <boruchor011@gmail.com>
2025-04-27 20:45:50 +0700

    Fix deployment configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/luxo-vista/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
